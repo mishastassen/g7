fdfd629f79b100c06972d68a4bce1ba7eb1bdcec
mishastassen <mishaspam@yahoo.com>
2015-11-13 13:50:20 +0100

    speler met platformen gemaakt

---
+++ Bond/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject Player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = this.transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdate() {
+		this.transform.position = Player.transform.position + offset;
+	}
+}
---
+++ Bond/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playercontroller : MonoBehaviour {
+
+	public float speedFactor;
+
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	bool IsGrounded() {
+		return Mathf.Abs (rb.velocity.y) < 0.1;
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		//rb.AddForce (speedFactor*movement);
+		float yVelocity = rb.velocity.y;
+		if(Input.GetButtonDown("Jump") && IsGrounded())
+			yVelocity = 10.0f;
+		rb.velocity = new Vector3(speedFactor*moveHorizontal, yVelocity, 0.0f);
+	}
+}
