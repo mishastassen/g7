fe657985873ec9cab48bc7197bbe29ed7036254a
jorism1993 <jorismollinga@hotmail.com>
2015-11-24 22:23:24 +0100

    De enemy loopt eindelijk normaal. Wat een gedoe

--- Core Gameplay/Minor Project/Assets/Scripts/EnemyController.cs
+++ Core Gameplay/Minor Project/Assets/Scripts/EnemyController.cs
@@ -6,78 +6,52 @@ public class EnemyController : NetworkBehaviour {
 
 	public Rigidbody enemy;
 	private float speed;
-	private bool justflipped;
-	private int flipcounter;
 
-	private bool toe;
-	private bool heel;
+	Vector3 middle;
+	float distance;
 
 	void Start () {
 		enemy = GetComponent<Rigidbody>();
-		speed = 5;
-		justflipped = false;
-		flipcounter = 0;
-
-		//toe = true;
-		//heel = true;
-
+		speed = 4;
+		middle = enemy.transform.position;
 	}
 
-	void FixedUpdate () {
-
-		if (justflipped) {
-			float yVelocity = enemy.velocity.y;
-			Vector3 movement = new Vector3 (speed, yVelocity, 0.0f); 
-			enemy.velocity = movement;
-			flipcounter++;
-			if (flipcounter > 40){
-				flipcounter = 0;
-				justflipped = false;
-			}
-		} 
-		else if (!justflipped) {
-			toe = isGroundedToe ();
-			Debug.Log (toe);
-			heel = isGroundedHeel ();
-			if (toe && heel) {
-				float yVelocity = enemy.velocity.y;
-				Vector3 movement = new Vector3 (speed, yVelocity, 0.0f); 
-				enemy.velocity = movement;
-			} 
-			else {
-				toe = true;
-				heel = true;
-				justflipped = true;
-				Flip ();
-			}
+	void Update () {
+		distance = Vector3.Distance (middle, enemy.transform.position);
+		if (distance < 9.5f) {
+			walkNormal ();
+		} else {
+			flip ();
+			Debug.Log ("Flipped at " + distance);
+			walkNormal ();
+			walkNormal ();
+			walkNormal ();
 		}
+	}
+
+	void walkNormal (){
+		enemy.transform.Translate (Vector3.down * speed * Time.deltaTime);
 	}
 
 	// checks whether the front of the enemy is on a platform
 	bool isGroundedToe() {
-		Vector3 toePosition = new Vector3 (enemy.transform.position.x + 2.0f, enemy.transform.position.y, enemy.transform.position.z);
-		return Physics.Raycast (toePosition, Vector3.down, 20f);
-
+		Vector3 toePosition = new Vector3 (enemy.transform.position.x , enemy.transform.position.y, enemy.transform.position.z);
+		return Physics.Raycast (toePosition, Vector3.up, 1.0f);
 	}
 
 	// checks whether the back of the enemy is on a platform
 	bool isGroundedHeel() {
-		Vector3 heelPosition= new Vector3 (enemy.transform.position.x - 2.0f, enemy.transform.position.y, enemy.transform.position.z);
-		return Physics.Raycast (heelPosition, Vector3.down, 20f);
-
+		Vector3 heelPosition= new Vector3 (enemy.transform.position.x , enemy.transform.position.y, enemy.transform.position.z);
+		return Physics.Raycast (heelPosition, Vector3.up, 1.0f);
 	}
 
-	void Flip() {
-		Debug.Log ("flipped");
+	void flip () {
 		float yRotation = enemy.transform.eulerAngles.y;
 		if (yRotation == 90) {
 			yRotation = 270;
-			speed = -5;
 		} else {
 			yRotation = 90;
-			speed = 5;
 		}		
 		enemy.transform.eulerAngles = new Vector3 (270, yRotation, 0);
-
 	}
 }
