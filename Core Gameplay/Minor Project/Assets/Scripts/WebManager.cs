856d642d747254d714b4c0a2b027909a2180f552
ggjbakker <geert.bakker@hotmail.com>
2015-12-08 11:10:27 +0100

    WebManager test

---
+++ Core Gameplay/Minor Project/Assets/Scripts/WebManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebManager : MonoBehaviour {
+
+	void Start(){
+		StartCoroutine (getUrl ());
+	}
+
+	IEnumerator getUrl(){
+		Debug.Log ("getting url");
+		WWW www = new WWW ("http://drproject.twi.tudelft.nl:8088");
+		yield return www;
+		Debug.Log (www.text);
+	}
+
+}
