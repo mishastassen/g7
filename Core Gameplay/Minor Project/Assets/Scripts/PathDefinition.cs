a40d0b04993f53734062c781fe328cd50dd8a137
mishastassen <mishaspam@yahoo.com>
2015-12-09 20:31:43 +0100

    Another conflict

--- Core Gameplay/Minor Project/Assets/Scripts/PathDefinition.cs
+++ Core Gameplay/Minor Project/Assets/Scripts/PathDefinition.cs
@@ -4,15 +4,15 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class PathDefinition : NetworkBehaviour {
-
+	
 	public List<Transform> points = new List<Transform>();
-
+	
 	public IEnumerator<Transform> pathEnumerator() {
 		int index = 0;
 		int dir = 1;
 		while (true) {
 			yield return points[index];
-
+			
 			if(index<=0)
 				dir = 1;
 			else if(index+1>=points.Count)
@@ -20,11 +20,11 @@ public class PathDefinition : NetworkBehaviour {
 			index+=dir;
 		}
 	}
-
-
+	
+	
 	public void OnDrawGizmos() {
 		for (int i=0; i+1<points.Count; i++)
 			Gizmos.DrawLine (points [i].position, points [i + 1].position);
 	}
-
+	
 }
