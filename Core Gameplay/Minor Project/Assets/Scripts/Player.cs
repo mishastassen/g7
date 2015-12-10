efe52bfe37cca4e859695c3d3d381e549689ab46
jorism1993 <jorismollinga@hotmail.com>
2015-12-10 15:14:59 +0100

    De update van de tweede minigame + wat onnodige dingen verwijderd

--- Core Gameplay/Minor Project/Assets/Scripts/Player.cs
+++ Core Gameplay/Minor Project/Assets/Scripts/Player.cs
@@ -16,14 +16,9 @@ public class Player : MonoBehaviour {
 	
 	// walkspeed, windspeed, rotatespeed, blowdir, severity
 	public float windspeed;
-	private float fallspeed;
 	private float blowdir;
 	private float severity;
 	private float compensate;
-	private float way_left;
-	private float way_right;
-	private bool falling_left;
-	private bool falling_right;
 	
 	// is the wind blowing?
 	private bool windBlowing;
@@ -36,20 +31,15 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		// leftanim = gameObject.GetComponentInChildren<Animator> ();
+		// rightanim = gameObject.GetComponentInChildren<Animator> ();
+		// controller = GetComponent<CharacterController> ();
 		severity = 1.0f;
-		fallspeed = 3.0f;
 		compensate = 1.5f;
 		frame_count = 1;
 		blowdir = getWindDirection ();
-
 		toleft.enableEmission = false;
 		toright.enableEmission = false;
-
-		way_left = 1f;
-		way_right = 1f;
-
-		falling_left = false;
-		falling_right = false;
 	}
 	
 	// Update is called once per frame
@@ -79,8 +69,8 @@ public class Player : MonoBehaviour {
 				toright.Play ();
 			}
 			
-			left.transform.RotateAround (LeftPlayerRot, Vector3.forward, blowdir * severity * windspeed * Time.deltaTime * way_left);
-			right.transform.RotateAround (RightPlayerRot, Vector3.forward, blowdir * severity * windspeed * Time.deltaTime * way_right);
+			left.transform.RotateAround (LeftPlayerRot, Vector3.forward, blowdir * severity * windspeed * Time.deltaTime);
+			right.transform.RotateAround (RightPlayerRot, Vector3.forward, blowdir * severity * windspeed * Time.deltaTime);
 			
 			if (wind_count > 40) {
 				windBlowing = false;
@@ -94,122 +84,106 @@ public class Player : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.W)) {
 			leftanim.SetBool ("WalkForward", true);
-			left.transform.Translate (Vector3.forward * walkspeed * Time.deltaTime * way_left);
+			left.transform.Translate (Vector3.forward * walkspeed * Time.deltaTime);
 		} else {
 			leftanim.SetBool ("WalkForward", false);
 		}
 		
 		if (Input.GetKey (KeyCode.S)) {
 			leftanim.SetInteger ("WalkBackwards", 1);
-			left.transform.Translate (-Vector3.forward * walkspeed * Time.deltaTime * way_left);
+			left.transform.Translate (-Vector3.forward * walkspeed * Time.deltaTime);
 		} else {
 			leftanim.SetInteger ("WalkBackwards", 0);
 		}
 
 		if (Input.GetKey (KeyCode.A)) {
-			left.transform.RotateAround(LeftPlayerRot,Vector3.forward, 1.5f * compensate*windspeed*Time.deltaTime * way_left);
+			left.transform.RotateAround(LeftPlayerRot,Vector3.forward, 1.5f * compensate*windspeed*Time.deltaTime);
 		}
 		
 		if (Input.GetKey (KeyCode.D)) {
-			left.transform.RotateAround(LeftPlayerRot,Vector3.forward, -1.5f * compensate*windspeed*Time.deltaTime * way_left);
+			left.transform.RotateAround(LeftPlayerRot,Vector3.forward, -1.5f * compensate*windspeed*Time.deltaTime);
 		}
 
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			rightanim.SetBool ("WalkForward", true);
-			right.transform.Translate (Vector3.forward * walkspeed * Time.deltaTime * way_right);
+			right.transform.Translate (Vector3.forward * walkspeed * Time.deltaTime);
 		} else {
 			rightanim.SetBool ("WalkForward", false);
 		}
 		
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			rightanim.SetInteger ("WalkBackwards", 1);
-			right.transform.Translate (-Vector3.forward * walkspeed * Time.deltaTime * way_right);
+			right.transform.Translate (-Vector3.forward * walkspeed * Time.deltaTime);
 		} else {
 			rightanim.SetInteger ("WalkBackwards", 0);
 		}
 		
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			right.transform.RotateAround(RightPlayerRot,Vector3.forward, 1.5f * compensate*windspeed*Time.deltaTime * way_right);
+			right.transform.RotateAround(RightPlayerRot,Vector3.forward, 1.5f * compensate*windspeed*Time.deltaTime);
 		}
 
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			right.transform.RotateAround(RightPlayerRot,Vector3.forward, -1.5f * compensate*windspeed*Time.deltaTime * way_right);
+			right.transform.RotateAround(RightPlayerRot,Vector3.forward, -1.5f * compensate*windspeed*Time.deltaTime);
 		}
 
 		if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.UpArrow)) {
 			leftanim.SetBool ("WalkForward", true);
-			left.transform.Translate (Vector3.forward * walkspeed * Time.deltaTime * way_left);
+			left.transform.Translate (Vector3.forward * walkspeed * Time.deltaTime);
 			rightanim.SetBool ("WalkForward", true);
-			right.transform.Translate (Vector3.forward * walkspeed * Time.deltaTime * way_right);
+			right.transform.Translate (Vector3.forward * walkspeed * Time.deltaTime);
 		}
 
 		// increase the severity of the wind if one progresses
-		if (left.transform.position.z > 10 | right.transform.position.z > 10) {
+		if (transform.position.z > 10) {
 			severity = 1.25f;
 			compensate = severity * 1.4f;
 		}
 		
-		if (left.transform.position.z > 20 | right.transform.position.z > 20) {
+		if (transform.position.z > 20) {
 			severity = 1.5f;
 			compensate = severity * 1.3f;
 		}
 		
-		if (left.transform.position.z > 30 | right.transform.position.z > 30) {
+		if (transform.position.z > 30) {
 			severity = 2.0f;
 			compensate = severity * 1.1f;
 		}
 
 		// check to see when the left player falls
 		if (left.transform.eulerAngles.z < 345 && left.transform.eulerAngles.z > 100) {
-			falling_left = true;
 			leftanim.SetBool ("FallRight" ,true);
+			left.transform.Translate( Vector3.down * 3 * Time.deltaTime);
 		}
 
 		if (left.transform.eulerAngles.z > 15 && left.transform.eulerAngles.z < 200) {
-			falling_left = true;
 			leftanim.SetBool ("FallRight" ,true);
+			left.transform.Translate( Vector3.down * 3 * Time.deltaTime);
 		}
 
-		if (falling_left) {
-			left.transform.Translate( Vector3.down * fallspeed * Time.deltaTime);
-		}
-
-		// check to see when the right player falls 
+		// check to see when the right player falls
 		if (right.transform.eulerAngles.z < 345 && right.transform.eulerAngles.z > 100) {
-			falling_right = true;
 			rightanim.SetBool ("FallRight" ,true);
+			right.transform.Translate( Vector3.down * 3 * Time.deltaTime);
 		}
 		
 		if (right.transform.eulerAngles.z > 15 && right.transform.eulerAngles.z < 200) {
-			falling_right = true;
 			rightanim.SetBool ("FallRight" ,true);
+			right.transform.Translate( Vector3.down * 3 * Time.deltaTime);
 		}
-
-		if (falling_right) {
-			right.transform.Translate( Vector3.down * fallspeed * Time.deltaTime);
-		}
-
-		// has the end been reached yet?
-		if (left.transform.position.z > 39) {
-			way_left = 0f;
-		}
-
-		if (right.transform.position.z > 39) {
-			way_right = 0f;
-		}
-		// end of update function below
 	}
 
 	float getWindDirection(){
 		blowdir = Random.Range (-1.0f, 1.0f);
 		
 		if (blowdir < 0) {
+			Debug.Log ("wind should be blowing");
 			blowdir = -1;
 		} else if (blowdir > 0) {
 			blowdir = 1;
 		} else {
 			blowdir = getWindDirection();
 		}
+		// blowdir = 0;
 		return blowdir;
 	}
 }
