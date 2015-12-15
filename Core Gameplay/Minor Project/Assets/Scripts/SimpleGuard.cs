1a8c64b64cf23ed6948f145a8026e24d2679753a
jorism1993 <jorismollinga@hotmail.com>
2015-12-15 14:07:59 +0100

    Domme guard die jou vindt nadat hij geinitaliseerd is

---
+++ Core Gameplay/Minor Project/Assets/Scripts/SimpleGuard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleGuard : MonoBehaviour {
+
+	public GameObject enemy;
+	private GameObject player;
+	NavMeshAgent agent;
+	private Transform PlayerPos;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponentInParent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		PlayerPos = player.transform;
+		agent.destination = PlayerPos.position;
+	}
+}
