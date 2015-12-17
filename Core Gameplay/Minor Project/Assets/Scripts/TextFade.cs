b5b8a1d966015b92eca09e3fdde89c1449ac56ff
jorism1993 <jorismollinga@hotmail.com>
2015-12-17 15:11:01 +0100

    Instructies in level1 en de minigame

---
+++ Core Gameplay/Minor Project/Assets/Scripts/TextFade.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour {
+
+	public Text firstinstruction;
+	public Text secondinstruction;
+
+	// Use this for initialization
+	void Start () {
+		secondinstruction.enabled = false;
+		StartCoroutine (firstTextFade ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator firstTextFade(){
+		yield return new WaitForSeconds (6f);
+		firstinstruction.enabled = false;
+		yield return new WaitForSeconds (3f);
+		secondinstruction.enabled = true;
+		yield return new WaitForSeconds (6f);
+		secondinstruction.enabled = false;
+	}
+}
