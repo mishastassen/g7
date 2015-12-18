9f2eea0a3f8a113e124a450d39b6f61627a0a6b5
mishastassen <mishaspam@yahoo.com>
2015-12-18 18:20:59 +0100

    Swoosh! What's stealth, when you have a sword.

---
+++ Core Gameplay/Minor Project/Assets/Scripts/SwordSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class SwordSwitcher : NetworkBehaviour {
+
+	public GameObject SwoldOnHip;
+	public GameObject SwoldInHand;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnGrab() {
+		Debug.Log ("Sword grabbed: "+SwoldOnHip.name);
+		SwoldOnHip.SetActive (false);
+		SwoldInHand.SetActive (true);
+	}
+
+	public void OnInPocket() {
+		Debug.Log ("Sword in pocket.");
+		SwoldOnHip.SetActive (true);
+		SwoldInHand.SetActive (false);
+	}
+}
