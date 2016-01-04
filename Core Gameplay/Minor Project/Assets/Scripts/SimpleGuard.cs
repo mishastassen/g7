60da7b017c6cc54cb000d7144f6bb95651cc7915
jorism1993 <jorismollinga@hotmail.com>
2016-01-04 16:08:12 +0100

    Update van de guard en level4

--- Core Gameplay/Minor Project/Assets/Scripts/SimpleGuard.cs
+++ Core Gameplay/Minor Project/Assets/Scripts/SimpleGuard.cs
@@ -1,5 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using UnityEngine.Analytics;
 
 public class SimpleGuard : MonoBehaviour {
 
@@ -7,16 +12,57 @@ public class SimpleGuard : MonoBehaviour {
 	private GameObject player;
 	NavMeshAgent agent;
 	private Transform PlayerPos;
+	private Vector3 ResetLoc;
+
+	private List <Collider> TriggerList= new List<Collider>();
 
 	// Use this for initialization
 	void Start () {
 		agent = GetComponentInParent<NavMeshAgent> ();
+		ResetLoc = enemy.transform.position;
+
 	}
 	
 	// Update is called once per frame
+//	void Update () {
+//		player = GameObject.FindGameObjectWithTag ("Player");
+//		PlayerPos = player.transform;
+//		agent.destination = PlayerPos.position;
+//	}
+
 	void Update () {
-		player = GameObject.FindGameObjectWithTag ("Player");
-		PlayerPos = player.transform;
-		agent.destination = PlayerPos.position;
+		foreach (Collider c in TriggerList) {
+			if (c.tag == "Player") {
+				Debug.Log ("Player found!");
+				player = GameObject.FindGameObjectWithTag ("Player");
+				PlayerPos = player.transform;
+				agent.destination = PlayerPos.position;
+				return;
+			} 
+		}
+		Debug.Log ("Should start walking back now!");
+		agent.destination = ResetLoc;	
+	}
+
+	void OnTriggerEnter (Collider other){
+
+		if (!TriggerList.Contains (other)) {
+			//add the object to the list
+			TriggerList.Add (other);
+		}
+
+		if (other.tag == "Player") {
+			Debug.Log ("Player entered!");
+		} 
+	}
+
+	void OnTriggerExit (Collider other){
+		//if the object is in the list of triggers, remove it
+		if (TriggerList.Contains (other)) {
+			if (other.tag == "Player") {
+				Debug.Log ("Player removed!");
+			}
+			TriggerList.Remove (other);
+		}
 	}
 }
