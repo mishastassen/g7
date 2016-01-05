868a99424fe499090b9d8c1c587b609113604e59
vlieger93 <vlieger93@live.nl>
2016-01-05 11:07:21 +0100

    minigame3 update

---
+++ Core Gameplay/Minor Project/Assets/Scripts/Minigame3/Obstacle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour
+{
+    public float movementspeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, movementspeed);
+    }
+}
---
+++ Core Gameplay/Minor Project/Assets/Scripts/Minigame3/Playermini3.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Playermini3 : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpducktime;
+    public Text livestext;
+
+    private static int lives;
+    private float timeLeft;
+    private bool jumping;
+    private bool ducking;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jumping = false;
+        ducking = false;
+        GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1);
+        timeLeft = 0;
+        lives = 30;
+        SetLives();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // horizontal movement
+        float moveHorizontal = Input.GetAxis("Horizontal_P1"); // change later
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        rb.transform.Translate(movement * speed);
+
+        //timer
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1);
+            ducking = false;
+            jumping = false;
+        }
+
+        //duck
+        if (Input.GetKeyDown("down") && !ducking)
+        {
+            timeLeft = jumpducktime;
+            GetComponent<Renderer>().material.color = new Color(0.5f, 0, 1);
+            ducking = true;
+            jumping = false;
+        }
+
+        //jump
+        if (Input.GetKeyDown("up") && !jumping)
+        {
+            timeLeft = jumpducktime;
+            GetComponent<Renderer>().material.color = new Color(0.5f, 1, 0);
+            jumping = true;
+            ducking = false;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            LoseLife();
+        }
+
+        if (other.gameObject.CompareTag("Bridge"))
+        {
+            if (!ducking)
+            {
+                LoseLife();
+            }
+        }
+        if (other.gameObject.CompareTag("Root"))
+        {
+            if (!jumping)
+            {
+                LoseLife();
+            }
+        }
+    }
+
+    void SetLives()
+    {
+        livestext.text = "Lives: " + lives.ToString();
+    }
+
+    void LoseLife()
+    {
+        lives = lives - 1;
+        SetLives();
+    }
+}
+
+
