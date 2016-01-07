1661c9ab86edabad88154b8a709fb2c934c81797
jorism1993 <jorismollinga@hotmail.com>
2016-01-07 17:33:54 +0100

    Kleine updates van Joris

---
+++ Core Gameplay/Minor Project/Assets/Scripts/FindingGuard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using UnityEngine.Analytics;
+
+public class FindingGuard : MonoBehaviour {
+
+	private GameObject player;
+	NavMeshAgent agent;
+	private Transform PlayerPos;
+
+	private List <Collider> TriggerList= new List<Collider>();
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponentInParent<NavMeshAgent> ();
+	}
+
+	void Update () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		PlayerPos = player.transform;
+		agent.destination = PlayerPos.position;
+	}
+
+	void OnTriggerEnter (Collider other){
+		if (!TriggerList.Contains (other)) {
+			//add the object to the list
+			TriggerList.Add (other);
+		}
+	}
+
+	void OnTriggerExit (Collider other){
+		//if the object is in the list of triggers, remove it
+		if (TriggerList.Contains (other)) {
+			if (other.tag == "Player") {
+			}
+			TriggerList.Remove (other);
+		}
+	}
+}
