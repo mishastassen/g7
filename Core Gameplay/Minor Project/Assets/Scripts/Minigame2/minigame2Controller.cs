631ccb76663d6d6d8b292b52acd10482ffae15e6
ggjbakker <geert.bakker@hotmail.com>
2016-01-07 15:07:35 +0100

    minigames multiplayer geschikt gemaakt

---
+++ Core Gameplay/Minor Project/Assets/Scripts/Minigame2/minigame2Controller.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class minigame2Controller : NetworkBehaviour {
+
+	// walkspeed, windspeed, rotatespeed, blowdir, severity
+	public float windspeed;
+	[SyncVar][HideInInspector]
+	public float blowdir;
+	[SyncVar][HideInInspector]
+	public float severity;
+	[SyncVar][HideInInspector]
+	public float compensate;
+	int frame_count;
+	[SyncVar][HideInInspector]
+	int wind_count;
+
+	// is the wind blowing?
+	[SyncVar][HideInInspector]
+	public bool windBlowing;
+	public ParticleSystem toleft;
+	public ParticleSystem toright;
+
+	[HideInInspector]
+	public GameObject left;
+	[HideInInspector]
+	public GameObject right;
+
+	void Start () {
+		severity = 1.0f;
+		compensate = 1.5f;
+		frame_count = 1;
+		toleft.enableEmission = false;
+		toright.enableEmission = false;
+
+		if (isServer) {
+			blowdir = getWindDirection ();
+		}
+	}
+
+	
+
+	void Update () {
+		if (isServer) {
+			frame_count++;
+			if (frame_count % 100 == 0) {
+				windBlowing = true;
+			}
+
+			if (windBlowing && wind_count > 40) {
+				windBlowing = false;
+				wind_count = 0;
+				blowdir = getWindDirection();
+				// geluid.Stop ();
+			}
+
+			// increase the severity of the wind if one progresses
+			if (left != null && right != null) {
+				if (left.transform.position.z > 15 | right.transform.position.z > 15) {
+					severity = 1.25f;
+					compensate = severity * 1.4f;
+				}
+
+				if (left.transform.position.z > 40 | right.transform.position.z > 40) {
+					severity = 1.5f;
+					compensate = severity * 1.3f;
+				}
+
+				if (left.transform.position.z > 60 | right.transform.position.z > 60) {
+					severity = 2.0f;
+					compensate = severity * 1.1f;
+				}
+			}
+		}
+
+		if (windBlowing) {
+			// geluid.PlayOneShot(wind,1.0f);
+			wind_count++;
+			if (blowdir > 0) {
+				toleft.enableEmission = true;
+				toleft.Play ();
+			} else {
+				toright.enableEmission = true;
+				toright.Play ();
+			}
+		} else {
+			toleft.enableEmission = false;
+			toright.enableEmission = false;
+		}
+	}
+
+	float getWindDirection(){
+		blowdir = Random.Range (-1.0f, 1.0f);
+
+		if (blowdir < 0) {
+			blowdir = -1;
+		} else if (blowdir > 0) {
+			blowdir = 1;
+		} else {
+			blowdir = getWindDirection();
+		}
+		return blowdir;
+	}
+}
--- Core Gameplay/Minor Project/Assets/Scripts/Minigame3/Playermini3.cs
+++ Core Gameplay/Minor Project/Assets/Scripts/Minigame3/Playermini3.cs
@@ -1,22 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 using UnityEngine.UI;
 
-public class Playermini3 : MonoBehaviour
+public class Playermini3 : NetworkBehaviour
 {
 
     public float speed;
     public float jumpducktime;
-    public Text livestext;
 
-    private static int lives;
     private float timeLeft;
     private bool jumping;
     private bool ducking;
 
     private Rigidbody rb;
 
-    // Use this for initialization
+	private Vector3 horizontalMovement;
+	private miniGame3Controller minigame3controller;
+
+	private int playerID = 1;
+
+	//Input
+	private string horizontalAxis = "Horizontal_P1";
+	private string jumpButton = "Jump_P1";
+	private string interact1Button = "Interact1_P1";
+	private string interact2Button = "Interact2_P1";
+	private string throwButton = "Throw_P1";
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,79 +34,85 @@ public class Playermini3 : MonoBehaviour
         ducking = false;
         GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1);
         timeLeft = 0;
-        lives = 30;
-        SetLives();
+		minigame3controller = GameObject.FindGameObjectWithTag ("minigame3controller").GetComponent<miniGame3Controller> ();
+
+		if (gameObject.GetComponent<NetworkIdentity> ().playerControllerId == 2) {
+			playerID = 2;
+			horizontalAxis = "Horizontal_P2";
+			jumpButton = "Jump_P2";
+			interact1Button = "Interact1_P2";
+			interact2Button = "Interact2_P2";
+			throwButton = "Throw_P2";
+		}
     }
 
-    // Update is called once per frame
+	void Update(){
+		if (isLocalPlayer) {
+			//timer
+			timeLeft -= Time.deltaTime;
+			if (timeLeft < 0) {
+				GetComponent<Renderer> ().material.color = new Color (0.5f, 1, 1);
+				ducking = false;
+				jumping = false;
+			}
+
+			//duck
+			if (Input.GetButtonDown(interact1Button) && !ducking) {
+				timeLeft = jumpducktime;
+				GetComponent<Renderer> ().material.color = new Color (0.5f, 0, 1);
+				ducking = true;
+				jumping = false;
+			}
+
+			//jump
+			if (Input.GetButtonDown(jumpButton) && !jumping) {
+				timeLeft = jumpducktime;
+				GetComponent<Renderer> ().material.color = new Color (0.5f, 1, 0);
+				jumping = true;
+				ducking = false;
+			}
+		}
+	}
+		
     void FixedUpdate()
     {
-
-        // horizontal movement
-        float moveHorizontal = Input.GetAxis("Horizontal_P1"); // change later
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
-        rb.transform.Translate(movement * speed);
-
-        //timer
-        timeLeft -= Time.deltaTime;
-        if (timeLeft < 0)
-        {
-            GetComponent<Renderer>().material.color = new Color(0.5f, 1, 1);
-            ducking = false;
-            jumping = false;
-        }
-
-        //duck
-        if (Input.GetKeyDown("down") && !ducking)
-        {
-            timeLeft = jumpducktime;
-            GetComponent<Renderer>().material.color = new Color(0.5f, 0, 1);
-            ducking = true;
-            jumping = false;
-        }
-
-        //jump
-        if (Input.GetKeyDown("up") && !jumping)
-        {
-            timeLeft = jumpducktime;
-            GetComponent<Renderer>().material.color = new Color(0.5f, 1, 0);
-            jumping = true;
-            ducking = false;
-        }
+		if (isLocalPlayer) {
+			// horizontal movement
+			float moveHorizontal = Input.GetAxis (horizontalAxis);
+			Vector3 horizontalMovement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+			rb.transform.Translate (horizontalMovement * speed);
+			horizontalMovement = Vector3.zero;
+		}
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Obstacle"))
         {
-            LoseLife();
+            CmdLoseLife();
         }
 
         if (other.gameObject.CompareTag("Bridge"))
         {
             if (!ducking)
             {
-                LoseLife();
+                CmdLoseLife();
             }
         }
         if (other.gameObject.CompareTag("Root"))
         {
             if (!jumping)
             {
-                LoseLife();
+                CmdLoseLife();
             }
         }
     }
 
-    void SetLives()
-    {
-        livestext.text = "Lives: " + lives.ToString();
-    }
-
-    void LoseLife()
-    {
-        lives = lives - 1;
-        SetLives();
+	[Command]
+    void CmdLoseLife()
+    {	
+		minigame3controller.lives -= 1;
+		minigame3controller.SetLives();
     }
 }
 
