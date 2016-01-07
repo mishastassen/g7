016427cc2d4e745f3a3a295f423721a4856e054c
rvannamen <rickvannamen@hotmail.com>
2016-01-07 16:00:27 +0100

    merge???

---
+++ Core Gameplay/Minor Project/Assets/Scripts/Minigame3/miniGame3Controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class miniGame3Controller : NetworkBehaviour {
+
+//	[SyncVar(hook="SetLives")]
+	public int lives = 30;
+	public Text livestext;
+
+	void Start(){
+		SetLives ();
+	}
+
+	public void SetLives()
+	{
+		livestext.text = "Lives: " + lives.ToString();
+	}
+
+}
