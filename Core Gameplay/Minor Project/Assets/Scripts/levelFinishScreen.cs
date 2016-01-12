bb35767b424dc12e7c4df1fa85ef898380bb0339
ggjbakker <geert.bakker@hotmail.com>
2016-01-12 10:55:03 +0100

    Highscores working and level progress saved in database and player preferences

--- Core Gameplay/Minor Project/Assets/Scripts/levelFinishScreen.cs
+++ Core Gameplay/Minor Project/Assets/Scripts/levelFinishScreen.cs
@@ -4,12 +4,14 @@ using UnityEngine.Networking;
 using SimpleJSON;
 using System.Collections;
 
-public class levelFinishScreen : NetworkBehaviour {
+public class levelFinishScreen : MonoBehaviour {
 
 	[HideInInspector]
 	public string nextLevel;
 	public GameObject highscorePanel;
+	public GameObject personalscorePanel;
 	public GameObject highscoreEntryPrefab;
+	public Text headline;
 
 
 	void OnEnable(){
@@ -17,8 +19,15 @@ public class levelFinishScreen : NetworkBehaviour {
 		int levelId = GameObject.Find("LevelManager").GetComponent<Levelvariables>().levelId;
 		if (WebManager.Instance.currentUser != null && NetworkServer.active) {
 			WebManager.Instance.updateHighscores (levelId, highscore);
+			WebManager.Instance.getHighscores (levelId, this);
+			if (WebManager.Instance.currentUser.levelProgress < levelId) {
+				WebManager.Instance.updateLevelProgress (levelId);
+			}
 		}
-		WebManager.Instance.getHighscores (levelId, this);
+		if (PlayerPrefs.GetInt("levelProgress") == null || PlayerPrefs.GetInt("levelProgress") < levelId) {
+			PlayerPrefs.SetInt ("levelProgress", levelId);
+		}
+		headline.text = "You finished the level in " + highscore + " seconds!";
 	}
 
 	public void nextLevelButton(){
@@ -28,14 +37,20 @@ public class levelFinishScreen : NetworkBehaviour {
 	public void displayHighscores(JSONNode highscores){
 		JSONNode top10 = highscores ["top10"];
 		JSONNode bestTime = highscores ["bestTime"][0];
-		foreach( var key in top10.Keys )
+		for (int i = 0; i<10; i++)
 		{
-			string highscoreText = highscores ["top10"] [key] ["Player1"] + " and " + highscores ["top10"] [key] ["Player2"] + " in " + highscores ["top10"] [key] ["Highscore"] +" seconds" ;
-			GameObject highscoreEntry = Instantiate (highscoreEntryPrefab) as GameObject;
-			highscoreEntry.transform.SetParent (highscorePanel.transform, false);
-			string spot = (string)key;
-			highscoreEntry.GetComponent<highScoreDisplay> ().displayHighscore (spot, highscoreText);
+			if (top10 [i] != null) {
+				string highscoreText = top10 [i] ["Player1"] + " and " + top10 [i] ["Player2"] + " in " + top10 [i] ["Highscore"] + " seconds";
+				GameObject highscoreEntry = Instantiate (highscoreEntryPrefab) as GameObject;
+				highscoreEntry.transform.SetParent (highscorePanel.transform, false);
+				string spot = (i+1).ToString ();
+				highscoreEntry.GetComponent<highScoreDisplay> ().displayHighscore (spot, highscoreText);
+			}
 		}
-
+		string personalscoreText = bestTime ["Player1"] + " and " + bestTime["Player2"] + " in " + bestTime["Highscore"] + " seconds";
+		string personalSpot = bestTime["position"];
+		GameObject personalscoreEntry = Instantiate (highscoreEntryPrefab) as GameObject;
+		personalscoreEntry.transform.SetParent (personalscorePanel.transform, false);
+		personalscoreEntry.GetComponent<highScoreDisplay> ().displayHighscore (personalSpot, personalscoreText);
 	}
 }
