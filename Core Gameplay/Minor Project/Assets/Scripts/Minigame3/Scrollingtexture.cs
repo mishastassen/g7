a876862137759c306df08b4f9a727ac416af042b
vlieger93 <vlieger93@live.nl>
2016-01-14 11:26:47 +0100

    minigame3 assets

---
+++ Core Gameplay/Minor Project/Assets/Scripts/Minigame3/Scrollingtexture.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scrollingtexture : MonoBehaviour {
+
+    public float speed;
+
+	void Update () {
+        gameObject.GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0f, Time.time * speed);
+	}
+}
