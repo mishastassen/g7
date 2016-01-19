3a8853da111d8a49b4af2a3af8d42ede357d33ac
rvannamen <rickvannamen@hotmail.com>
2016-01-19 09:20:48 +0100

    hints level 1c (weet nog niet of het werkt)

---
+++ Core Gameplay/Minor Project/Assets/Scripts/UI/DoorInstructionController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DoorInstructionController : MonoBehaviour {
+
+	private int playerCount;
+	private bool doorOpen;
+	private bool eventEnabled;
+
+	public Text doorInstruction;
+	public Image instructionPlank;
+
+	void Start () {
+		playerCount = 0;
+		doorOpen = false;
+		doorInstruction.enabled = false;
+		instructionPlank.enabled = false;
+		Eventmanager.Instance.EventonSwitchPulled += SwitchPulled;
+		eventEnabled = true;
+		Gamemanager.Instance.onDisableEventHandlers += OnDisable;
+	}
+	
+	void Update () {
+		if (playerCount > 0 && !doorOpen) {
+			doorInstruction.enabled = true;
+			instructionPlank.enabled = true;
+		}
+	}
+
+	void SwitchPulled (int id) {
+		doorOpen = true;
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			playerCount += 1;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.tag == "Player") {
+			playerCount -= 1;
+		}
+	}
+
+	void OnDisable(){
+		if (eventEnabled) {
+			Eventmanager.Instance.EventonSwitchPulled -= SwitchPulled;
+			eventEnabled = false;
+		}
+	}
+}
